c238fe2ce0fdc006d0430c422b2f75c53f0603ee
readyadam <133392811+readyadam@users.noreply.github.com>
2023-05-12 20:18:10 +0200

    Delete main.js

--- main.js
+++
@@ -1,5 +0,0 @@
-const jatekInditasButton = document.getElementById('jatek-inditas-btn');
-
-jatekInditasButton.addEventListener('click', function() {
-  window.location.href = 'https://readyadam.github.io/Game/';
-});
