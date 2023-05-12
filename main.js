48cd4e56f5333af591da8eaef919e90e4890acd5
readyadam <133392811+readyadam@users.noreply.github.com>
2023-05-12 19:58:19 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
+window.open('https://readyadam.github.io/Game/', '_blank');
+
 const button = document.querySelector('button');
 button.addEventListener('click', function() {
   window.open('https://readyadam.github.io/Game/', '_blank');
 });
 
 startGameBtn.addEventListener('click', function() {
-    const startGameBtn = document.getElementById('https://readyadam.github.io/Game/');
+const startGameBtn = document.getElementById('https://readyadam.github.io/Game/');
 });
+
